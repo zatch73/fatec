92b6c4c7974877b36af89f59ee075347cf995801
zatch73 <zatch73@duck.com>
2023-05-20 02:04:38 -0300

    backup pi

---
+++ Projeto Integrador/portifolio/src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar">
+      <div class="container">
+        <div id="navMenu" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="#">Home</a>
+            <a class="navbar-item" href="#">About</a>
+            <a class="navbar-item" href="#">Projects</a>
+            <a class="navbar-item" href="#">Contact</a>
+          </div>
+          <div class="navbar-end">
+          </div>
+        </div>
+      </div>
+    </nav>
+</template>
